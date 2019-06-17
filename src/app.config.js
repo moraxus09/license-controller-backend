4c3ef9235987c34e925f3ca6d0ae130bf21b865d
Ivan Makhlynets <vanya.makhlinets@gmail.com>
2019-06-17 15:35:31 -0700

    huynya

--- src/app.config.js
+++ src/app.config.js
@@ -1,4 +1,6 @@
 module.exports = {
     secret: 'Secret',
     mongoUrl: 'mongodb://admin:password123@ds263156.mlab.com:63156/license-controller',
+    customSearchApiKey: 'AIzaSyA_dfqmlGA9l3fWYiLNWR5keyBpO0zXt7Y',
+    
 }
