cb368dbf7a0981dd058aef94171f67a2dac46c3f
Ivan Makhlynets <vanya.makhlinets@gmail.com>
2019-06-17 15:42:40 -0700

    huynya

--- src/products/products.router.js
+++ src/products/products.router.js
@@ -11,16 +11,5 @@ router.put('/:id', authCheck, multer({ storage: documentsStorage }).array('docum
 router.delete('/:id', authCheck, productsCtrl.deleteProduct);
 router.get('/:id/messages', authCheck, productsCtrl.getMessages);
 router.post('/:id/messages', authCheck, productsCtrl.sendMessage);
-router.get('/test', (req, res) => {
-    fetch('https://www.googleapis.com/customsearch/v1?key=AIzaSyA_dfqmlGA9l3fWYiLNWR5keyBpO0zXt7Y&cx=008584258193415137263:7c1dnhw7mlk&q=car&callback=callback');
-})
-
-function callback(res) {
-    const msg = {
-        text: JSON.stringify(res),
-        productId: 'dsadsa',
-        sender: 'USER'
-    }
-}
 
 module.exports = router;
